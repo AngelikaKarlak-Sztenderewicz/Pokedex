3b6ea73bcb5e406bba7bf328709418828dbf9a7f
Angelika Karlak-Sztenderewicz <angelika.karlak@o2.pl>
2025-09-10 22:59:35 +0200

    Empty main

--- FrontEnd/src/components/shared/Button/Button.jsx
+++
@@ -1,7 +0,0 @@
-import { ButtonStyle } from "./Button.styles";
-
-const Button = ({ children, ...rest }) => {
-  return <ButtonStyle {...rest}>{children}</ButtonStyle>;
-};
-
-export default Button;
